1d4e11d924772cf88eb54a5bc0ea55207c8a1882
Jaibw <jai.unix@gmail.com>
2022-06-09 15:52:08 +0530

    Create install-minikube.sh

---
+++ install-minikube.sh
@@ -0,0 +1,6 @@
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo dpkg -i minikube_latest_amd64.deb
+minikube start --driver=docker
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl get po -A
