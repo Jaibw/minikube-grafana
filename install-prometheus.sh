3a5154fa172ceefa90c9464a4f3c10a8614e8e24
Jaibw <jai.unix@gmail.com>
2022-06-15 10:23:07 +0530

    Update install-prometheus.sh

--- install-prometheus.sh
+++ install-prometheus.sh
@@ -1,5 +1,6 @@
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm install prometheus prometheus-community/prometheus
 kubectl expose service prometheus-server --type=NodePort --target-port=9090 --name=prometheus-server-np
+sleep 120
 minikube service prometheus-server-np --url
 curl `minikube service prometheus-server-np --url`
