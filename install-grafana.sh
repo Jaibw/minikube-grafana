f319675e49726a93ddf83ddf216db89e1ce996bb
Jaibw <jai.unix@gmail.com>
2022-06-15 10:10:47 +0530

    Update install-grafana.sh

--- install-grafana.sh
+++ install-grafana.sh
@@ -2,7 +2,7 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm install grafana grafana/grafana
 kubectl expose service grafana --type=NodePort --target-port=3000 --name=grafana-np
 minikube service grafana --url
-echo 'Password:'
-kubectl get secret --namespace default grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
 sleep 120
 nohup kubectl port-forward --address 0.0.0.0 svc/grafana-np 31000:80 &
+echo 'Password:'
+kubectl get secret --namespace default grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
